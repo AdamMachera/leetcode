54c3ad795f7fe46a1dcf097a558ec3fe2275344f
amachera <adam.machera@gmail.com>
2016-10-11 12:57:33 +0200

    Added IsNumber solution

--- Leetcode.Tests/IsNumberManagerTester.cs
+++ Leetcode.Tests/IsNumberManagerTester.cs
@@ -105,5 +105,249 @@ namespace Leetcode.Tests
             var res = mgr.IsNumber(val);
             Assert.IsFalse(res);
         }
+
+
+        [TestMethod]
+        public void IsOnlyE()
+        {
+            string val = "e";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyEOnBegining()
+        {
+            string val = "e14";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyEOnEnd()
+        {
+            string val = "14e";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyEOnBeginingWithPlus()
+        {
+            string val = "+14e";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyEOnBeginingWithMinus()
+        {
+            string val = "-14e";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyEOnEndWithPlus()
+        {
+            string val = "+14e";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyEOnEndWithMinus()
+        {
+            string val = "-14e";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyOneValidEMinus()
+        {
+            string val = "-14e1";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsTwoInvalidEMinus()
+        {
+            string val = "-14ee1";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsOnlyDot()
+        {
+            string val = ".";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsDotWithPreceedingNumber()
+        {
+            string val = "5.";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsTwoDotWithPreceedingNumber()
+        {
+            string val = "5..";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsNumberWithWhitespacesOnEnd()
+        {
+            string val = " 5 ";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsNumberWithWhitespacesOnEnd2()
+        {
+            string val = " 5.  ";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsDotWithEpsilon()
+        {
+            string val = ".e1";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsDotWithEpsilonAndPlus()
+        {
+            string val = " +.e1";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsDotWithEpsilonAndDotAtTheEnd()
+        {
+            string val = " 1e.1";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void OnlyPluses()
+        {
+            string val = "+++";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void MinusDotAndV()
+        {
+            string val = "-.V";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void NumberDotAndEpsilonAndInteger()
+        {
+            string val = "46.e3";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void NumberDotIntegerAndEpsilonAndInteger()
+        {
+            string val = "46.2e3";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void NumberDotAndEpsilon()
+        {
+            string val = "46.e";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void DotNumberAndEpsilonInteger()
+        {
+            string val = ".2e81";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void EpsilonPlusNumber()
+        {
+            string val = " 005047e+6";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void EpsilonMinusNumber()
+        {
+            string val = " 005047e-6";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IntegerEpsilonPlus()
+        {
+            string val = "4e+";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IntegerDotMinusInteger()
+        {
+            string val = "32.e-80123";
+            IsNumberManager mgr = new IsNumberManager();
+            var res = mgr.IsNumber(val);
+            Assert.IsTrue(res);
+        }
     }
 }
--- Leetcode/IsNumberManager.cs
+++ Leetcode/IsNumberManager.cs
@@ -10,14 +10,20 @@ namespace Leetcode
     {
         static char[] validIntegers = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
         static char[] forbidden = new char[] { 'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-        'p','r','s','t','u','w','x','y','z',
+        'p','q','r','s','t','u', 'v', 'w','x','y','z',
         'A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-        'P','R','S','T','U','W','X','Y','Z', '-', ' ', ':', '\\', '{', };
+        'P','Q','R','S','T','U', 'V', 'W','X','Y','Z', '-', ' ', ':', '\\', '{', '-', '+'};
 
         public bool IsNumber(string s)
         {
             char[] chars = s.ToCharArray();
             char[] withoutWhiteSpace = this.RemoveWhitespaces(chars);
+            withoutWhiteSpace = this.RemoveWhitespacesOnEnd(withoutWhiteSpace);
+
+            if (withoutWhiteSpace.Length == 0)
+            {
+                return false;
+            }
 
             char[] withoutPlusMinusSign = null;
             if (withoutWhiteSpace[0] == '-' || withoutWhiteSpace[0] == '+')
@@ -42,15 +48,34 @@ namespace Leetcode
             int dotCounter = 0;
             int epsilonCounter = 0;
 
+            int epsilonPosition = 0;
+            int dotPosition = 0;
+
+            if (items[0] == '.' && items.Length == 1)
+            {
+                return true;
+            }
+
+            if (items.Length > 1 && items[0] == '.' && (items[1] == 'e' || items[1] == 'E'))
+            {
+                return true;
+            }
+
+            if (items[0] == 'e' || items[0] == 'E' || items[items.Length - 1] == 'e' || items[items.Length - 1] == 'E')
+            {
+                return true;
+            }
+
             for (int i = 0; i < items.Length; i++)
             {
-                if (forbidden.Contains(items[i]))
+                if (forbidden.Contains(items[i]) && !this.IsPartOfEpsilon(items[i], items, i))
                 {
                     return true;
                 }
 
                 if (items[i] == 'e' || items[i] == 'E')
                 {
+                    epsilonPosition = i;
                     epsilonCounter++;
                     if (epsilonCounter > 1)
                     {
@@ -60,11 +85,27 @@ namespace Leetcode
 
                 if (items[i] == '.')
                 {
+                    dotPosition = i;
                     dotCounter++;
                     if (dotCounter > 1)
                     {
                         return true;
                     }
+                }
+            }
+
+            if (epsilonCounter == 1 && dotCounter == 1)
+            {
+                if (dotPosition < epsilonPosition && 
+                    ((dotPosition != 0 && validIntegers.Contains(items[dotPosition - 1])) || dotPosition == 0)
+                    && ((epsilonPosition < items.Length - 1 && validIntegers.Contains(items[epsilonPosition + 1]) || items[epsilonPosition + 1] == '-' || items[epsilonPosition + 1] == '+'))
+                    )
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
                 }
             }
 
@@ -96,6 +137,31 @@ namespace Leetcode
             return items;
         }
 
+        private char[] RemoveWhitespacesOnEnd(char[] items)
+        {
+            int counter = items.Length;
+            for (int i = items.Length - 1; i >= 0; i--)
+            {
+                if (items[i] == ' ')
+                {
+                    counter--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (counter < items.Length)
+            {
+                char[] withoutWhitespaces = new char[counter];
+                this.CopyCharsFromBegining(items, counter, ref withoutWhitespaces);
+                return withoutWhitespaces;
+            }
+
+            return items;
+        }
+
         private void CopyChars(char[] items, int itemsToSkip, ref char[] result)
         {
             int position = 0;
@@ -104,6 +170,37 @@ namespace Leetcode
                 result[position] = items[i];
                 position++;
             }
+        }
+
+        private void CopyCharsFromBegining(char[] items, int counter, ref char[] result)
+        {
+            int position = 0;
+            for (int i = 0; i < counter; i++)
+            {
+                result[position] = items[i];
+                position++;
+            }
+        }
+
+        private bool IsPartOfEpsilon(char c, char[] items, int index)
+        {
+            if (c != 'e' && c != 'E' && c != '+' && c != '-')
+            {
+                return false;
+            }
+
+            if ((items[index] == 'e' || items[index] == 'E') && index + 1 < items.Length && (items[index + 1] == '+' || items[index + 1] == '-'))
+            {
+                return true;
+            }
+
+            if ((items[index] == '+' || items[index] == '-') && index - 1 >= 0 && (items[index - 1] == 'e' || items[index - 1] == 'E') && index + 1 < items.Length)
+            {
+                return true;
+            }
+
+
+            return false;
         }
     }
 }
